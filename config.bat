c33abb7d699947f56de1e5b66f0c39e3cb079ae8
andrehideki <andre.hkiw.dev@gmail.com>
2023-04-14 16:23:23 -0300

    add: config.bat > configure python dependencies on virtual environment

---
+++ config.bat
@@ -0,0 +1,10 @@
+python -m venv venv_win_teste
+call venv_win_teste/Scripts/activate
+pip install cx-Oracle==8.3.0
+pip install oracledb==1.3.0
+pip install Pillow==9.5.0
+pip install pyinstaller==5.9.0
+pip install pywin32==306
+pip install pywin32-ctypes==0.2.0
+pip install SQLAlchemy==2.0.9
+pip install tk==0.1.0
